0165823b8567e3153d575a7d37d096b6af5135af
WilliamCPaul <84932940+WilliamCPaul@users.noreply.github.com>
2021-05-28 08:12:48 +0100

    Implemented BetterBasket

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -13,6 +13,17 @@ public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
   
-
+  @Override
+  public boolean add( Product pr )
+  {  
+	  for(int i=0;i<this.size();i++) {
+		  if(this.get(i).getProductNum().equals(pr.getProductNum())) {
+			  this.get(i).setQuantity(this.get(i).getQuantity()+pr.getQuantity());
+			  return true;
+		  }
+	  }
+	  super.add(pr);
+	  return true;
+  }
   // You need to add code here
 }
